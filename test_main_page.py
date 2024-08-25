e3438a8912e8b21fdaaccdf3a7404ce4961d56f9
magic.kch <magic.kch@ya.ru>
2024-08-25 13:26:01 +0300

    add Page Object structure

--- test_main_page.py
+++ test_main_page.py
@@ -1,16 +1,15 @@
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
-
-def wait_element(browser, delay_seconds=10, by=By.CLASS_NAME, value=None):
-    return WebDriverWait(browser, delay_seconds).until(
-        EC.presence_of_element_located((by, value))
-    )
+from pages.main_page import MainPage
 
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = wait_element(browser, by=By.CSS_SELECTOR, value="#login_link")
-    login_link.click()
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
